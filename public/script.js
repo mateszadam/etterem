e64793e90fe5a432d7aed0b9ccd69b04518c6750
mateszadam <matesz.adam@gmail.com>
2024-03-25 17:41:54 +0100

    MA-Kevinnek előkészítve

--- public/script.js
+++ public/script.js
@@ -1,2 +1,19 @@
 "use strict";
-let ohistory = [];
+let ordered = [];
+fetchDataFromMenü();
+function fetchDataFromMenü() {
+    fetch("http://localhost:3000/rendeles")
+        .then((response) => response.json())
+        .then((data) => {
+        ordered = data;
+        main();
+    });
+}
+function main() {
+    console.log(ordered);
+    // itt van az ordered tömböd amiben a rendelések vannak
+    // ide ird azt amivel valahogy megjeleníted őket.
+    // Ezt a két paranyot irt be két külőn terminalba:
+    // npx json-server -w ./etterem.json
+    // npx tsc -w 
+}
--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,40 @@
-
-
-interface History {
-    id: string;
-    date: string;
-    items: Kosar[];
+interface Kosar {
+  id: string;
+  etelid: string;
+  db: number;
 }
 
+interface Rendeles {
+  id?: string;
+  date: string;
+  nev: string;
+  cim: string;
+  tel: string;
+  email: string;
+  etelek: Kosar[];
+}
 
-let ohistory: History[] = [];
+let ordered: Rendeles[] = [];
+
+fetchDataFromMenü();
+function fetchDataFromMenü() {
+  fetch("http://localhost:3000/rendeles")
+    .then((response) => response.json())
+    .then((data) => {
+      ordered = data;
+      main();
+    });
+}
+
+function main() {
+  console.log(ordered);
+    // itt van az ordered tömböd amiben a rendelések vannak
+    // ide ird azt amivel valahogy megjeleníted őket.
 
 
+    // Ezt a két paranyot irt be két külőn terminalba:
+    // npx json-server -w ./etterem.json
+    // npx tsc -w 
 
+
+}
